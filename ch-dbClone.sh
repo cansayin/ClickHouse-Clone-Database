7157c55bac30962f8bc5f8d2d546d0a2e0858cf7
cansayin <46593013+cansayin@users.noreply.github.com>
2022-11-11 22:34:44 +0300

    Update ch-dbClone.sh

--- ch-dbClone.sh
+++ ch-dbClone.sh
@@ -5,7 +5,14 @@ echo ""
 echo "######### ClickHouse Databases ###########"
 echo "------------------------------------------"
 
-clickhouse-client --password yourpassword --query 'show databases Format CSV;'
+echo "Please enter ClickHouse User Name"
+read -p "User Name : " username
+
+echo "Please enter ClickHouse Password"
+read -p "Password : " password
+
+clickhouse-client --user ${username} --password ${password} --query 'show databases Format CSV;'
+
 echo Please choose a database name:
 read -p "Database Name :" database_name
 
@@ -13,8 +20,8 @@ echo Please write a branch name:
 read -p "Clone Name : " branch_name
 
 
-clickhouse-client --password yourpassword --query "BACKUP DATABASE ${database_name} TO Disk('backups','${database_name}.${branch_name}.zip')"
+clickhouse-client --user ${username} --password ${password} --query "BACKUP DATABASE ${database_name} TO Disk('backups','${database_name}.${branch_name}.zip')"
 echo "INFO: ${database_name} Backup Has Been Taken"
 
-clickhouse-client --password yourpassword --query "RESTORE DATABASE ${database_name} AS ${database_name}_${branch_name} FROM Disk('backups','${database_name}.${branch_name}.zip')"
+clickhouse-client --user ${username} --password ${password} --query "RESTORE DATABASE ${database_name} AS ${database_name}_${branch_name} FROM Disk('backups','${database_name}.${branch_name}.zip')"
 echo "INFO: ${database_name}_${branch_name} Restore Finished"
