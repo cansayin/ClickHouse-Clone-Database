f04902d7ec00d3475df61d12d684c1274ce694ff
cansayin <46593013+cansayin@users.noreply.github.com>
2023-01-07 21:32:03 +0300

    Update ch-dbClone.sh

--- ch-dbClone.sh
+++ ch-dbClone.sh
@@ -15,6 +15,15 @@ echo "------------------------------------------"
 
 clickhouse-client --user ${username} --password ${password} --query 'show databases Format CSV;'
 
+echo ""
+echo "Please Select Branching Option"
+echo "To Create Branch WITH DATA Press '1'"
+echo "To Create Branch WITHOUT DATA Press '2'"
+echo ""
+read -p "Your Choice : " chosen_statement
+
+if [ $chosen_statement == 1 ]
+then
 echo Please choose a database name:
 read -p "Database Name : " database_name
 
@@ -27,3 +36,16 @@ echo "INFO: ${database_name} Backup Has Been Taken"
 
 clickhouse-client --user ${username} --password ${password} --query "RESTORE DATABASE ${database_name} AS ${database_name}_${branch_name} FROM Disk('backups','${database_name}.${branch_name}.zip')"
 echo "INFO: ${database_name}_${branch_name} Restore Finished"
+
+elif [ $chosen_statement == 2 ]
+then
+echo Please choose a database name:
+read -p "Database Name : " database_name
+
+echo Please write a Clone name:
+read -p "Clone Name : " branch_name
+
+clickhouse-client --user ${username} --password ${password} --query "CREATE DATABASE ${database_name}_${branch_name}"
+echo "INFO: ${database_name}_${branch_name} sucessfully createad WITHOUT DATA"
+
+fi
